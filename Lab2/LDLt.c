cb10c41cfc0e83c1e49043b754423ea3bb850e64
Yevgeniy Sudakov <yevgeniysudakov@gmail.com>
2021-10-08 23:58:32 +0300

    fix: ldlt c code v2

--- Lab2/LDLt.c
+++ Lab2/LDLt.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define n 3
+#define swap(a, b){double tmp = a; a = b; b = tmp; }
 
 
-
-void printMatrix(float matrix[][n]) {
-	printf("--------- matrix--------\n");
+void printMatrix(double matrix[n][n]) {
+	printf(" ------------------\n");
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			printf("%f ", matrix[i][j]);
@@ -14,35 +14,97 @@ void printMatrix(float matrix[][n]) {
 	}
 }
 
+void printVector(double matrix[n]) {
+	printf("--------------------------\n");
+	for (int i = 0; i < n; i++) {
+		printf("%f\n", matrix[i]);
+	}
+}
 
-int main(void) {
-	float A[n][n] = { {3,2,1 }, {2,4,2}, {1,2,5} };
-	float L[n][n] = { 0 };
-	float D[n][n] = { 0 };
+//transporance of matrix
+//void T(double matrix[][n]) {
+//	for (int j = 0; j < n; j++) {
+//		for (int i = 0; i <= j; i++) {
+//			if (i == j) {
+//				continue;
+//			}
+//			swap(matrix[i][j], matrix[j][i]);
+//		}
+//	}
+//}
 
-	printMatrix(A);
-	float sum = 0;
-	D[0][0] = A[0][0];
-	//method LDLt factorization
+void T(double L[n][n], double Lt[n][n]) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			Lt[i][j] = L[j][i];
+		}
+	}
+}
+
+
+void ld(double A[n][n],double L[n][n],double D[n][n]) {// LDLt factorization
+	double sum;
 	for (int j = 0; j < n; j++) {
 		for (int i = j; i < n; i++) {
 			sum = A[i][j];
 			for (int k = 0; k < i; k++) {
-				sum = sum - L[i][k] * L[j][k] * D[k][k];
+				sum -= L[i][k] * L[j][k] * D[k][k];
 			}
 			if (i == j) {
 				D[j][j] = sum;
 				L[i][j] = 1;
 			}
-			else 
+			else
 				L[i][j] = sum / D[j][j];
-
 		}
 	}
-	printMatrix(L);
-	printMatrix(D);
 
-	
+}
+
+
+void ldltSolve(double  L[n][n],double D[n][n],double Lt[n][n], double B[n], double X[n]) {
+	double z[n];
+	double y[n];
+	// Lz=B
+	for (int i = 0; i < n; i++) {
+		double alpha = B[i];
+		for (int j = 0; j < i; j++)
+			alpha -= L[i][j] * z[j];
+		z[i] = alpha / L[i][i];
+	}
+	printVector(z);
+	// Dy=z
+	for (int i = 0; i < n; i++) {
+		y[i] = z[i] / D[i][i];
+	}
+	printVector(y);
+	// Ltz = x
+	for (int i = n - 1; i >= 0; i--) {
+		double alpha =y[i];
+		for (int j = n - 1; j > i; j--)
+			alpha -= Lt[i][j] * X[j];
+		X[i] = alpha / Lt[i][i];
+	}
+	printVector(X);
+}
+
+
+int main(void) {
+	double A[n][n] = { {3,2,1 }, {2,4,2}, {1,2,5} };
+	double L[n][n] = { 0 };
+	double D[n][n] = { 0 };
+	double Lt[n][n];
+	double b[n] = { 5, 1, 2 };
+	double x[n];
+	printMatrix(A);
+	//solving ldlt
+
+	printVector(b);
+	ld(A, L, D);
+	T(L, Lt);
+	printMatrix(Lt);
+	ldltSolve(L, D,Lt, b, x);
+	printVector(x);
 
 
 	return 0;
