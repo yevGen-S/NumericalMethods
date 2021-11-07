665d8d474b2d17fd1587f13d3cca1fcb011525fa
Yevgeniy Sudakov <yevgeniysudakov@gmail.com>
2021-11-08 02:54:29 +0300

    feat&fix: changes  - reimplement function of creating random symmetric matrix with defined cond  - fix function of writting to file  - fix function of reading from file  - add functions for creating graphics

---
+++ Lab2/functions.py
@@ -0,0 +1,81 @@
+import numpy as np
+import csv
+import random
+from decimal import Decimal
+
+N = 10
+
+
+# def create_rand_symetric_matrix_with_defined_cond(n, cond):
+#     log_cond = np.log(cond)
+#     exp_vec = np.arange(-log_cond / 4., log_cond * (n + 1) / (4 * (n - 1)), log_cond / (2. * (n - 1)))
+#     s = np.exp(exp_vec)
+#     S = np.diag(s)
+#     U, _ = np.linalg.qr((np.random.rand(n, n) - 5.) * 200)
+#     V, _ = np.linalg.qr((np.random.rand(n, n) - 5.) * 200)
+#     A = U.dot(S).dot(V.T)
+#     A = A.dot(A.T)
+#     A = (A + A.T) / 2
+#     print(np.linalg.cond(A)-cond)
+#     return A
+
+# def create_rand_matrix_with_defined_cond(n, cond):
+#     log_cond = np.log(cond)
+#     exp_vec = np.arange(-log_cond / 4., log_cond * (n + 1) / (4 * (n - 1)), log_cond / (2. * (n - 1)))
+#     s = np.exp(exp_vec)
+#     S = np.diag(s)
+#     U, _ = np.linalg.qr((np.random.rand(n, n) - 5.) * 200)
+#     V, _ = np.linalg.qr((np.random.rand(n, n) - 5.) * 200)
+#     A = U.dot(S).dot(V.T)
+#     A = A.dot(A.T)
+#     return A
+
+
+def create_rand_symmetric_matrix_with_defined_cond(n, cond):
+    D = np.diag(np.ones(n))
+    D[n - 1][n - 1] = cond
+    Q, R = np.linalg.qr(np.random.random(size=(n, n)))
+    A = Q.dot(D).dot(Q.T)
+    A = (A + A.T) / 2
+    print(np.linalg.cond(A))
+    return A
+
+
+def create_rand_x_ex():
+    x_ex = []
+    for i in range(10):
+        x_ex.append(random.random())
+    return x_ex
+
+
+def write_to_file_matrix(list_of_matrices, name_of_file):
+    with open(name_of_file, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        if name_of_file == "A2.csv":
+            for row in range(10):
+                writer.writerow(list_of_matrices[row])
+            return 0
+        for i in range(16):
+            matrix = list_of_matrices[i]
+            if name_of_file == "b.csv" or name_of_file == "b2.csv": #if it's vector
+                writer.writerow(matrix)
+                writer.writerow('\t')
+                continue
+            for row in matrix:
+                writer.writerow(row)
+            writer.writerow('\t')
+
+    return 0
+
+
+def read_from_file(name_of_file):
+    array = []
+    with open(name_of_file, "r") as file:
+        reader = csv.reader(file, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+        for row in reader:
+            array.append(row)
+        for i in range(16):
+            array[i] = array[i][:-1]
+        x = np.array(array)
+
+    return x
--- Lab2/graphics.py
+++ Lab2/graphics.py
@@ -1,4 +1,97 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from matrix_with_defined_cond import create_rand_symetric_matrix_with_defined_cond
+import functions
 
+x_ex = np.ones(10)
+
+
+def create_Ab():
+    list_of_A = []
+    list_of_b = []
+    cond = 10
+    for i in range(16):
+        A = functions.create_rand_symetric_matrix_with_defined_cond(10, cond)
+        list_of_A.append(A)
+        b = A.dot(x_ex)
+        list_of_b.append(b)
+        cond *= 10
+
+    return list_of_A, list_of_b
+
+
+# Creating random  A matrices with cond from 10 till 10**16 and b vectors
+A, b = create_Ab()
+
+# writing to files matrices for using method
+functions.write_to_file_matrix(A, "A.csv")
+functions.write_to_file_matrix(b, "b.csv")
+
+# reading results of using method from file
+x = functions.read_from_file("X1.csv")
+
+
+def graph_err_of_cond():
+    x_rel_error = []
+    cond = []
+
+    for i in range(16):
+        x_rel_error.append(np.linalg.norm(x[i] - x_ex) / np.linalg.norm(x_ex))
+        print(np.linalg.norm(x[i] - x_ex))
+        cond.append(10 ** (i + 1))
+    plt.figure(1)
+    plt.grid()
+    plt.ylabel('||X-X_ex|| / ||X_ex||')
+    plt.xlabel('cond')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.plot(cond, x_rel_error)
+    plt.title("Error of cond number")
+
+
+# График зависимости относительной погрешности решений от числа обусловленности
+graph_err_of_cond()
+
+
+def data_for_2_graph():
+    list_of_b2 = []
+    A2 = functions.create_rand_symmetric_matrix_with_defined_cond(10, 100)
+    b2 = A2.dot(x_ex)
+    list_of_b2.append(b2)
+
+    for i in range(16):
+        b2 = b2 + 0.001
+        list_of_b2.append(b2)
+
+    functions.write_to_file_matrix(A2, "A2.csv")
+    functions.write_to_file_matrix(list_of_b2, "b2.csv")
+
+    return list_of_b2
+
+
+b2 = data_for_2_graph()[0]
+list_of_b2 = data_for_2_graph()
+
+
+def graph_err_of_b_changing():
+    x_rel_error2 = []
+    err_db = []
+
+    for i in range(16):
+        x_rel_error2.append(np.linalg.norm(x[i] - x_ex) / np.linalg.norm(x_ex))
+
+    for i in range(16):
+        err_db.append(np.linalg.norm(b2 - list_of_b2[i]) / np.linalg.norm(b2))
+
+    plt.figure(2)
+    plt.grid()
+    plt.ylabel('||X-X_ex|| / ||X_ex||')
+    plt.xlabel('||db|| / ||b||')
+    plt.yscale('log')
+    # plt.xscale('log')
+    plt.title("Error X of changing b ")
+    plt.plot(err_db, x_rel_error2)
+
+
+
+graph_err_of_b_changing()
+plt.show()
