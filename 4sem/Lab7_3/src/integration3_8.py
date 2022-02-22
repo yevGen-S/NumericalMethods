04443a030f99f3f6a405215748ef2fb44300b3db
Yevgeniy Sudakov <yevgeniysudakov@gmail.com>
2022-02-22 23:01:42 +0300

    feat: implement numerical integration method 3/8

---
+++ 4sem/Lab7_3/src/integration3_8.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+
+
+def func(x):
+    return x * np.sin(np.pi * x)
+
+
+def integration3_8_recursive(f, a, b, eps):
+    I_prev = (f(a) + 3 * f((2 * a + b) / 3) +
+              3 * f((a + 2 * b) / 3) + f(b)) * (b - a) / 8
+
+    I = ((f(a) + 3 * f((2 * a + (a + b) / 2) / 3) + 3 * f((a + 2 * (a + b) / 2) / 3) + f((a + b) / 2)) * (
+            (a + b) / 2 - a)) / 8 \
+        + (f(a) + 3 * f((2 * (a + b) / 2 + b) / 3) + 3 * f(((a + b) / 2 + 2 * b) / 3) + f(b)) * (b - (a + b) / 2) / 8
+
+    if (abs(I - I_prev) / (2 ** 4 - 1)) < eps:
+        return I_prev
+
+    return integration3_8_recursive(f, a, (a + b) / 2, eps) + integration3_8_recursive(f, (a + b) / 2, b, eps)
+
+
+def integration3_8(f, a, b, eps):
+    def integration_body(number_of_nodes):
+        x_grid = np.linspace(a, b, number_of_nodes)
+        n = len(x_grid)
+        I = 0
+        for i in range(1, n):
+            I += (f(x_grid[i - 1]) + 3 * f((2 * x_grid[i - 1] + x_grid[i]) / 3) +
+                  3 * f((x_grid[i - 1] + 2 * x_grid[i]) / 3) + f(x_grid[i])) * (x_grid[i] - x_grid[i - 1]) / 8
+        return I
+
+    incrementer = lambda i: i + (i - 1)
+    n = 2
+    I_prev = integration_body(n)
+    n = incrementer(n)
+    I = integration_body(n)
+
+    while (abs(I - I_prev) / (2 ** 4 - 1)) > eps:
+        I_prev = I
+        n = incrementer(n)
+        I = integration_body(n)
+
+    return I
+
+
+def fun(x):
+    return np.exp(x)
+
+
+print(integration3_8(fun, a=0, b=1, eps=1e-14))
+print(integrate.quad(fun, a=0, b=1))
